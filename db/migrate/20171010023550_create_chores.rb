5770947a850cd9e294804fd38e306d9f70017d21
Amy Grouse <agrouse@tulane.edu>
2017-10-10 18:47:50 +0000

    app, db , and test changes

---
+++ db/migrate/20171010023550_create_chores.rb
@@ -0,0 +1,14 @@
+class CreateChores < ActiveRecord::Migration
+  def change
+    create_table :chores do |t|
+      t.string "task"
+      t.string "priority"
+      t.string "status"
+      t.string "description"
+      t.string "person_assigned"
+      t.datetime "last_accomplished"
+      t.string "history"
+      t.timestamps
+    end
+  end
+end
