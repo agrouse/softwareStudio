f8a67417961a70d3dfd1cb6f8c0044b468a35204
Erin Blake <eblake1@tulane.edu>
2017-11-20 18:07:11 +0000

    Added password confirmation, still working on redirect to sign up upon failure

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,11 @@ class Person < ActiveRecord::Base
     has_secure_password
     belongs_to :household
     
+    # attr_accessor :password_confirmation
+    
+    # attr_accessor :user_name
+    # attr_protected :password
+    
     validates :first_name, :presence => true,
                             :length => { :within => 2..30 },
                             :format => /\A[a-zA-Z]+\Z/                        
@@ -17,4 +22,16 @@ class Person < ActiveRecord::Base
     
     validates :username, :presence => true,
                             :length => { :within => 3..30}
+                            
+    validates_confirmation_of :password, :message => "Password and confirmation should match"
+    
+    validates :password_confirmation, :presence => true, :if => '!password.nil?'
+    
+    # validates :password, 
+    #       # you only need presence on create
+    #       :presence => { :on => :create },
+    #       # allow_nil for length (presence will handle it on create)
+    #       :length   => { :minimum => 6, :allow_nil => true },
+    #       # and use confirmation to ensure they always match
+    #       :confirmation => true
 end
