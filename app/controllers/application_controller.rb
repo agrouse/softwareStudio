3cb2e4118589ba603e47d442a98f800e3d88d401
Amy Grouse <agrouse@tulane.edu>
2017-11-21 03:40:51 +0000

    Fixed dropdown to show only people belonging in your household

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
 
-  
-  def people_of_household()
-    #get user's id from currently logged in person
-    userID = session[:user_id]
-    #get house for associated with this user
-    person = Person.find(userID)
-    houseID = person.household_id
-    house = Household.find(houseID)
-    #get all users of this house
-    @peopleInHouse = house.people
-    @peopleInHouse.save
-  end
+  helper_method :people_of_household
+   def people_of_household
+     #get user's id from currently logged in person
+     userID = session[:user_id]
+  #   #get house for associated with this user
+     person = Person.find(userID)
+     houseID = person.household_id
+     house = Household.find(houseID)
+  #   #get all users of this house
+     peopleList = []
+
+    return peopleList
+   end
+   
   
   private
-  
     def confirm_logged_in
       unless session[:user_id]
         flash[:notice] = "Please log in."
         redirect_to(access_login_path)
       end
     end
+    
 end
