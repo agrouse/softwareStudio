f60c32c5761c399ae9b8a09648e29d00a6c372f1
Amy Grouse <agrouse@tulane.edu>
2017-11-21 02:29:48 +0000

    dropdown edits in chores view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  private
-  
-  def confirm_logged_in
-    unless session[:user_id]
-      flash[:notice] = "Please log in."
-      redirect_to(access_login_path)
-    end
-  end
+
   
   def people_of_household()
     #get user's id from currently logged in person
-    @user = Person.find(params[:id])
+    userID = session[:user_id]
     #get house for associated with this user
-    @houseID = @user.household_id
-    @house = Households.find(houseID)
+    person = Person.find(userID)
+    houseID = person.household_id
+    house = Household.find(houseID)
     #get all users of this house
     @peopleInHouse = house.people
-    return peopleInHouse
-    
+    @peopleInHouse.save
   end
+  
+  private
+  
+    def confirm_logged_in
+      unless session[:user_id]
+        flash[:notice] = "Please log in."
+        redirect_to(access_login_path)
+      end
+    end
 end
