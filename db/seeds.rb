7094bc9dd854591652376a123846008d120f4f22
Erin Blake <eblake1@tulane.edu>
2017-12-16 14:21:46 +0000

    Added test movie to pop up window

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+# In this file we have automated seeding of users. We have seeded 7 users and one household
 
-#chores = Chores.create!([{task: 'Clean dishes', priority: 'high', status: 'uncompleted'}, {task: 'Trash', priority: 'moderate', status: 'completed'}])
-house1 = Household.create!([{name: 'House 1'}])
+house1 = Household.create!([{name: '31 McAlister Dr'}])
 bryce = Person.create!([{first_name: 'Bryce', last_name: 'Bullock', username: 'greatguy22', password: "password", password_confirmation: "password", household_id: 1}])
 amy = Person.create!([{first_name: 'Amy', last_name:'Grouse', username: 'agrouse', password: "password", password_confirmation: "password", household_id: 2}])
 walter = Person.create!([{first_name: 'Walter', last_name:'Ogzaly', username: 'wogzaly', password: "password", password_confirmation: "password", household_id: 1}])
@@ -16,11 +17,3 @@ mackenna = Person.create!([{first_name: 'Mackenna', last_name:'Barker', descript
 emma = Person.create!([{first_name: 'Emma', last_name:'Peyko', username: 'epeyko', password: "password", password_confirmation: "password", household_id: 1}])
 adrien = Person.create!([{first_name: 'Adrien', last_name:'Underwood', username: 'aunderwood', password: "password", password_confirmation: "password", household_id: 1}])
 
- 
-#households = Household.create!([{name: 'House 1'}])
-# households = Household.create!([{name: 'House 2'}])
-# households = Household.create!([{name: 'House 3'}])
-# households = Household.create!([{name: 'House 4'}])
-#household = @person.household.create!([{name: 'House 5'}])
-#chores.save!
-# Person.create(name: 'Yana', chore: chores.first)
